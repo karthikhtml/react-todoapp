298fb9f50ae8a91b14826a46fb743f1e4852992a
karthikhtml <106761441+karthikhtml@users.noreply.github.com>
2023-02-23 18:06:20 +0530

    Updates

---
+++ precache-manifest.cbbb334e77ab3127e23e99771db695a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa61b5ebcadc7b52cea",
+    "url": "/react-todoapp/static/js/main.4aa61b5e.chunk.js"
+  },
+  {
+    "revision": "15fe7507691cd8d86ce4",
+    "url": "/react-todoapp/static/js/1.15fe7507.chunk.js"
+  },
+  {
+    "revision": "b1f04d793cf16cdaa992",
+    "url": "/react-todoapp/static/js/runtime~main.b1f04d79.js"
+  },
+  {
+    "revision": "35248d24505276368714ef97bfa87e35",
+    "url": "/react-todoapp/index.html"
+  }
+];
